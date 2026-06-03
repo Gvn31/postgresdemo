67d491b6879a0d5cd41476c04a9465f9987e3b77
Gvn31 <jivin.2201@gmail.com>
2026-06-03 18:41:20 +0530

    .env filr added and also established connection to db using db.py

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # postgresdemo
 
 # conda activate venv/
+
+# git add .
+
+# git config --global user.email "jivin.2201@gmail.com"
+# git commit -m "initial commit added req.txt,venv"
+# git push origin main
---
+++ db.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2 import sql, OperationalError
+import psycopg2.extras
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+conn=None
+
+#fn to connect to db
+
+def get_db_connection():
+    try:
+        conn=psycopg2.connect(
+            host=os.getenv("HOST"),
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD"),
+            port=os.getenv("PORT")
+        )
+        print("Database connection established")
+        return conn
+    
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        return conn
+    
+conn=get_db_connection()
